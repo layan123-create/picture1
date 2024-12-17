3a8bdd2c1be25c68a368dfaeac8b85e81f71c01d
layan123-create <layanawad123123@gmail.com>
2024-12-17 23:20:33 +0200

    first commit

--- app/src/main/java/com/example/picture1/MainActivity.java
+++ app/src/main/java/com/example/picture1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.picture1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +12,58 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView imageView2;
+    private int sum=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView2=findViewById(R.id.imageView);
+    }
+    public void arrowR(View view) {
+        if(sum==1){
+            sum++;
+            imageView2.setImageResource(R.drawable.lion);
+        }
+        else {
+            if (sum == 2) {
+                sum++;
+                imageView2.setImageResource(R.drawable.tiger);
+            } else {
+                if (sum == 3) {
+                    sum++;
+                    imageView2.setImageResource(R.drawable.ghazal);
+                } else {
+                    if (sum == 4) {
+                        sum++;
+                        imageView2.setImageResource(R.drawable.giraffe);
+                    }
+                }
+            }
+        }
+    }
+
+
+    public void arrowL (View view){
+        if (sum == 5) {
+            sum--;
+            imageView2.setImageResource(R.drawable.ghazal);
+        } else {
+            if (sum == 4) {
+                sum--;
+                imageView2.setImageResource(R.drawable.tiger);
+            } else {
+                if (sum == 3) {
+                    sum--;
+                    imageView2.setImageResource(R.drawable.lion);
+                } else {
+                    if (sum == 2) {
+                        sum--;
+                        imageView2.setImageResource(R.drawable.cat);
+                    }
+                }
+            }
+
+        }
     }
 }
